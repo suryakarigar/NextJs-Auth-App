7ed155e1669f6072dd1865a7f90eecde8703eb3f
Surya Karigar <karigarsurya@gmail.com>
2024-01-27 13:21:50 +0530

    Login part complete but user can still jump to pages using url (need fix)

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -2,13 +2,52 @@ import connect from "@/dbConfig/dbConfig";
 import User from "@/models/userModel"
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs"
+import jwt from "jsonwebtoken"
 
 connect() 
 
 export async function POST(request: NextRequest) {
     try {
-        
-    } catch (error) {
-        
+        const reqBody = await request.json()
+        const {email, password} = reqBody
+
+        console.log(reqBody) //Not a good idea on production level
+
+        // check if user already exists
+        const user = await User.findOne({email})
+
+        if(!user) {
+            return NextResponse.json({error: "User does not exist"}, {status: 400})
+        }
+
+        // check if password is correct
+        const validPassword = await bcryptjs.compare(password, user.password)
+        if(!validPassword) {
+            return NextResponse.json({ error: "Invalid Password"}, {status: 400})
+        }
+
+
+        // create token data
+        const tokenData = {
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+
+        // create token
+        const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, {expiresIn: '1d'})
+
+        const response = NextResponse.json( {
+            message: "Login Successful",
+            success: true
+        })
+        response.cookies.set("token", token, {
+            httpOnly: true,
+        })
+
+        return response;
+
+    } catch (error: any) {
+        return NextResponse.json({error: error.message}, {status: 500})
     }
 }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,34 +1,56 @@
 'use client'; //to make it frontend because everything is on server in NextJs
 
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation"; //To forcefully send the user to profile page
 import axios from "axios";
-
-
+import {toast} from "react-hot-toast";
 
 
 export default function LoginPage() {
-
+    const router = useRouter();
     const [user, setUser] = useState( {
         email: "",
         password: ""
     } )
 
+    const [buttonDisabled, setButtonDisabled] = React.useState(false);
+    const [loading, setLoading] = React.useState(false)
+
     // Always use async in method because it talks to the database
     const onLogin = async () => {
+        try {
+            setLoading(true)
+            const response = await axios.post("/api/users/login", user)
+            console.log("Login successful", response.data);
+            //send the user to Profile Page
+            router.push("/profile")
 
+        } catch (error: any) {
+            console.log("Login failed", error.message);
+           toast.error(error.message)
+        } finally {
+            setLoading(false)
+        }
     }
+
+    useEffect(() => {
+        if(user.email.length > 0 && user.password.length > 0) {
+            setButtonDisabled(false)
+        } else {
+            setButtonDisabled(true)
+        }
+    }, [user])
 
     return (
         <div className="flex flex-col justify-center items-center h-screen">
             
-            <h1 className=" text-2xl mb-5">Log in</h1>
+            <h1 className=" text-2xl mb-5">{loading ? "Processing" : "Login" }</h1>
             <hr />
             <div className="flex justify-between h-100 flex-col">
                 <div className="m-3">
                 <label htmlFor="email" className="m-3">Email</label>
-                <input className=" p-2 rounded-lg" 
+                <input className=" p-2 rounded-lg text-black" 
                     id="email"
                     type="text"
                     value={user.email}
@@ -38,7 +60,7 @@ export default function LoginPage() {
                 </div>
                 <div>
                 <label htmlFor="password" className="m-3">Password</label>
-                <input className=" p-2 rounded-lg" 
+                <input className=" p-2 rounded-lg text-black" 
                     id="password"
                     type="password"
                     value={user.password}
@@ -49,7 +71,7 @@ export default function LoginPage() {
             </div>
             <button 
             onClick={onLogin}
-            className="px-6 py-2 m-3 rounded-lg bg-slate-600">Log in</button>
+            className="px-6 py-2 m-3 rounded-lg bg-slate-600">{buttonDisabled ? "No Login" : "Login"}</button>
             <Link href="/signup">Visit Signup Page</Link>
         </div>
     )
